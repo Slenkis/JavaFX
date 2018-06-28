778c51cc76bde65297b3b946f1346565e83e7e23
Slenkis <zaharkin629@gmail.com>
2018-06-28 09:12:08 +0300

    main

--- src/sample/codeJava/main/Main.java
+++ src/sample/codeJava/main/Main.java
@@ -1,14 +1,8 @@
 package main;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 import scenes.SceneTemplates;
-
-
-import java.io.IOException;
 
 public class Main extends Application {
     private static Stage manageStage;
@@ -16,19 +10,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         manageStage = primaryStage;
-//        Parent rootAuth = null;
-//        try {
-//            rootAuth = FXMLLoader.load(getClass().getResource("/fxmls/authWindow.fxml"));
-//        } catch (IOException e) {
-//            System.out.println("fxml err main");
-//            e.printStackTrace();
-//        }
-//
-//        assert rootAuth != null;
-//        Scene sceneAuth = new Scene(rootAuth);
-//        primaryStage.setScene(sceneAuth);
+        try {
+            primaryStage.setScene(SceneTemplates.getSceneAuth());
+        } catch (Exception e) {
+            System.out.println("main 14");
+        }
 
-        primaryStage.setScene(SceneTemplates.getSceneAuth());
         primaryStage.setTitle("Subus");
         primaryStage.show();
     }
